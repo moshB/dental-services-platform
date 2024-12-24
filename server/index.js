4a4150534a184cca7a64905a371398803f5ef73c
mosh <moshebuyum11@gmail.com>
2024-12-24 18:18:53 +0200

    Ready for Heroku

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path'); // משמש להגשת קבצים סטטיים
 const dotenv = require('dotenv');
+// require('dotenv').config();
+
 const clinicsRoutes = require('./routes/clinicsRoutes');
 
 // טען משתני סביבה מקובץ .env
